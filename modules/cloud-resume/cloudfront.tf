f31f942b4a3758a000af8bfc5574799b052281d4
Tomi <48963553+CaliphKnaz@users.noreply.github.com>
2023-02-13 15:45:09 +0000

    adding cloudfront

---
+++ modules/cloud-resume/cloudfront.tf
@@ -0,0 +1,57 @@
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_origin_access_control" "resume" {
+  name                              = "resume-access"
+  description                       = "Acess to origin"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.resume-bucket.bucket_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.resume.id
+    origin_id                = local.s3_origin_id
+  }
+  
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+
+
+
